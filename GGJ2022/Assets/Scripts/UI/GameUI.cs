0cb87a832efe885f8aa1c01f546d837ba1584d38
ababba0 <ababba@sjtu.edu.cn>
2022-01-23 05:06:10 +0800

    230506_UI

--- GGJ2022/Assets/Scripts/UI/GameUI.cs
+++ GGJ2022/Assets/Scripts/UI/GameUI.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.UI;
 using Gameplay;
 
 public class GameUI : MonoBehaviour{
 	public string path="Assets\\GameAssets\\Textures\\UI\\White";
 	public string playerName="LeftGrid/Origin/PlayerL";
+	public int hpVal,skillCnt;
+	public float mpVal,spVal,spMax=5f;
 	
 //	public PlayerScript sc;
 	private Texture2D[] mp=new Texture2D[7],hp=new Texture2D[4],skill=new Texture2D[4];
@@ -15,7 +18,9 @@ public class GameUI : MonoBehaviour{
 	
     void Start(){
 		for(int i=0;i<=6;i++)mp[i]=(Texture2D)AssetDatabase.LoadAssetAtPath(path+"\\mp"+i+".png",typeof(Texture2D));
+		hp[0]=(Texture2D)AssetDatabase.LoadAssetAtPath("Assets\\GameAssets\\Textures\\Opaque.png",typeof(Texture2D));
 		for(int i=1;i<=3;i++)hp[i]=(Texture2D)AssetDatabase.LoadAssetAtPath(path+"\\hp"+i+".png",typeof(Texture2D));
+		skill[0]=(Texture2D)AssetDatabase.LoadAssetAtPath("Assets\\GameAssets\\Textures\\Opaque.png",typeof(Texture2D));
 		for(int i=1;i<=3;i++)skill[i]=(Texture2D)AssetDatabase.LoadAssetAtPath(path+"\\skill"+i+".png",typeof(Texture2D));
 		spBar=(Texture2D)AssetDatabase.LoadAssetAtPath(path+"\\spBar.png",typeof(Texture2D));
     }
@@ -26,6 +31,18 @@ public class GameUI : MonoBehaviour{
 	}
 	
     void Update(){
+		var mpTar=transform.Find("mp").GetComponent<RawImage>();
+		mpTar.texture=mp[Mathf.FloorToInt(mpVal)];
 		
+    	var hpTar=transform.Find("hp").GetComponent<RawImage>();
+		hpTar.texture=hp[hpVal];
+		
+    	var skillTar=transform.Find("skill").GetComponent<RawImage>();
+		skillTar.texture=skill[skillCnt];
+		
+    	var spMTar=transform.Find("spBarMid");
+		spMTar.localScale=new Vector3(0.3925f*spVal/spMax,0.08750676f,1);
+    	var spRTar=transform.Find("spBarRight");
+		spRTar.localPosition=new Vector3(39.23f*spVal/spMax-17.73f,-18.24f,0);
     }
 }
