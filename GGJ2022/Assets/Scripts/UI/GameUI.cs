1c25988a0217b60b3e090978ef325a0f45ec908f
UsernameCodeRookie <94690535+UsernameCodeRookie@users.noreply.github.com>
2022-01-23 15:11:57 +0800

    fix.资源获取方式修正

--- GGJ2022/Assets/Scripts/UI/GameUI.cs
+++ GGJ2022/Assets/Scripts/UI/GameUI.cs
@@ -7,25 +7,50 @@ using UnityEngine.UI;
 using Gameplay;
 
 public class GameUI : MonoBehaviour{
-	public string path="Assets\\GameAssets\\Textures\\UI\\White";
+//	public string path="Assets\\GameAssets\\Textures\\UI\\White";
 //	public string playerName="LeftGrid/Origin/PlayerL";
     public PlayerScript sc;
-
+	public string path;
 //	public int hpVal,skillCnt;
 //	public float mpVal,spVal,spMax=5f;
 	
 //	public PlayerScript sc;
+/*
 	private Texture2D[] mp=new Texture2D[7],hp=new Texture2D[4],skill=new Texture2D[4];
 	private Texture2D spBar;
+*/
+	public List<Texture2D> mp,hp,skill;
+	public Texture2D spBar;
+
 	RawImage mpTar, hpTar, skillTar;
 
     void Start(){
+		/*
 		for(int i=0;i<=6;i++)mp[i]=(Texture2D)AssetDatabase.LoadAssetAtPath(path+"\\mp"+i+".png",typeof(Texture2D));
 		hp[0]=(Texture2D)AssetDatabase.LoadAssetAtPath("Assets\\GameAssets\\Textures\\Opaque.png",typeof(Texture2D));
 		for(int i=1;i<=3;i++)hp[i]=(Texture2D)AssetDatabase.LoadAssetAtPath(path+"\\hp"+i+".png",typeof(Texture2D));
 		skill[0]=(Texture2D)AssetDatabase.LoadAssetAtPath("Assets\\GameAssets\\Textures\\Opaque.png",typeof(Texture2D));
 		for(int i=1;i<=3;i++)skill[i]=(Texture2D)AssetDatabase.LoadAssetAtPath(path+"\\skill"+i+".png",typeof(Texture2D));
 		spBar=(Texture2D)AssetDatabase.LoadAssetAtPath(path+"\\spBar.png",typeof(Texture2D));
+		*/
+		/*
+		for(int i = 0; i < 6; i++)
+        {
+			mp[i] = Resources.Load<Texture2D>(path + "mp" + i);
+		}
+		hp[0] = Resources.Load<Texture2D>(path + "Opaque.png");
+		for (int i = 1; i <= 3; i++)
+		{
+			hp[i] = Resources.Load<Texture2D>(path + "hp" + i + ".png");
+		}
+		skill[0] = Resources.Load<Texture2D>(path + "Opaque.png");
+		for (int i = 1; i <= 3; i++)
+		{
+			skill[i] = Resources.Load<Texture2D>(path + "skill" + i + ".png");
+		}
+		spBar = Resources.Load<Texture2D>(path + "spBar.png");
+		*/
+
 
 		mpTar = transform.Find("mp").GetComponent<RawImage>();
 		hpTar = transform.Find("hp").GetComponent<RawImage>();
