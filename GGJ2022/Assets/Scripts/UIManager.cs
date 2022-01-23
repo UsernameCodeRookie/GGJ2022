075c10a36990fcf72b9b59ee88d62118fc462d09
ababba0 <ababba@sjtu.edu.cn>
2022-01-23 08:36:15 +0800

    230836_UI

--- GGJ2022/Assets/Scripts/UIManager.cs
+++ GGJ2022/Assets/Scripts/UIManager.cs
@@ -1,24 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gameplay;
 
 public class UIManager:MonoBehaviour{
 	public GameObject startUI,endUI,playUI,pauseUI,gameMgr;
 	
+	
 	private int state;//0:start_1:end_2:play_3:pause
 	
-    void Start(){
+    void Awake(){
 		state=0;
 		startUI.SetActive(true);
 		endUI.SetActive(false);
 		playUI.SetActive(false);
 		pauseUI.SetActive(false);
 		gameMgr.SetActive(false);
+		gameMgr.GetComponent<GameManager>().GameOver.AddListener(GameEnd);
 	}
 	
     void Update(){
-//		if(state==2&&Input.GetKey("escape")){
-			
-//		}
+		if(state==2&&Input.GetKey("escape"))GamePause();
+	}
+	
+	public void GameReset(){
+		state=2;
+		startUI.SetActive(false);
+		endUI.SetActive(false);
+		playUI.SetActive(true);
+		pauseUI.SetActive(false);
+		gameMgr.SetActive(true);
+//		gameMgr.GetComponent<GameManager>().Reset();
+	}
+	
+	public void GameEnd(){
+		state=1;
+//		startUI.SetActive(false);
+		endUI.SetActive(true);
+//		playUI.SetActive(false);
+//		pauseUI.SetActive(false);
+		gameMgr.SetActive(false);
+	}
+	
+	public void GamePause(){
+		state=3;
+//		startUI.SetActive(false);
+//		endUI.SetActive(false);
+		playUI.SetActive(false);
+		pauseUI.SetActive(true);
+		gameMgr.SetActive(false);
+	}
+	
+	public void GameContinue(){
+		state=2;
+//		startUI.SetActive(false);
+//		endUI.SetActive(false);
+		playUI.SetActive(true);
+		pauseUI.SetActive(false);
+		gameMgr.SetActive(true);
+	}
+	
+	public void GameExit(){
+		Application.Quit();
 	}
 }
